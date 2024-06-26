e81bd31a97872e7c52e98df7ffb157d814204e02
samluddite <56632175+SamsRepos@users.noreply.github.com>
2024-06-26 18:10:59 +0000

    hash_dir fix, hashes now consistent between runs

--- dir_hasher.py
+++ dir_hasher.py
@@ -3,8 +3,13 @@ import hashlib
 
 # https://stackoverflow.com/questions/36204248/creating-unique-hash-for-directory-in-python
 
-def sha1_of_file(file_path):
-  sha = hashlib.sha1()
+def sha256_of_string(str):
+  sha = hashlib.new('sha256')
+  sha.update(str.encode())
+  return sha.hexdigest()
+
+def sha256_of_file(file_path):
+  sha = hashlib.new('sha256')
   with open(file_path, 'rb') as file:
     while True:
       block = file.read(2**10) # Magic number: one-megabyte blocks.
@@ -16,8 +21,8 @@ def hash_dir(dir_path):
   hashes = []
   for path, dirs, files in os.walk(dir_path):
     for file in sorted(files): # guarantee same order every time
-      hashes.append(sha1_of_file(os.path.join(path, file)))
+      hashes.append(sha256_of_file(os.path.join(path, file)))
     for dir in sorted(dirs): # guarantee same order every time 
       hashes.append(hash_dir(os.path.join(path, dir)))
     break # we only need one iteration - to get files and dirs
-  return str(hash(''.join(hashes)))
+  return str(sha256_of_string(''.join(hashes)))
