19e3693e625b6367dda2bf3e32f0ced4e1d8f1b9
samluddite <56632175+SamsRepos@users.noreply.github.com>
2024-06-27 19:40:38 +0000

    db oriented program structure

--- tables.sql
+++ tables.sql
@@ -1,14 +1,8 @@
-CREATE TABLE "dirs_to_hash" (
+CREATE TABLE "dirs" (
 	"id"	INTEGER NOT NULL,
 	"dir_path"	TEXT NOT NULL,
 	"active"	INTEGER NOT NULL,
+	"destination_paths"	TEXT NOT NULL,
+	"latest_hash"	TEXT,
 	PRIMARY KEY("id" AUTOINCREMENT)
-)
-
-
-CREATE TABLE "dir_hashes" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"dir_path"	TEXT NOT NULL,
-	"hash"	TEXT NOT NULL,
-	PRIMARY KEY("id")
 )
