ebc1aaf1a679a88782421ae918dfb6eaa25d34a0
samluddite <56632175+SamsRepos@users.noreply.github.com>
2024-07-06 14:24:41 +0000

    Cleaned up azb.py

--- azb.py
+++ azb.py
@@ -6,33 +6,32 @@ from modules.azb_engine import run_azb
 for i in range(len(argv)):
   print(f"Arg #{i}: {argv[i]}")
 
-RUN_ONCE_ARG = "run_once"
-RUN_DAILY_ARG = "run_daily"
-RUN_WHEN_DIRS_AVAILABLE_ARG = "run_when_dirs_available"
-CLI_ARG = "cli"
-GUI_ARG = "gui"
-
-VALID_ARGUMENTS = [
-  RUN_ONCE_ARG,
-  RUN_DAILY_ARG,
-  RUN_WHEN_DIRS_AVAILABLE_ARG
-]
+class VALID_ARGUMENTS:
+  RUN_ONCE                = "run_once"
+  RUN_DAILY               = "run_daily"
+  RUN_WHEN_DIRS_AVAILABLE = "run_when_dirs_available"
+  CLI                     = "cli"
+  GUI                     = "gui"
 
 if len(argv) != 2:
   raise ValueError("bad input. usage: python azb.py <valid argument>")
 
 # Arg #0: azb.py
 arg = argv[1]
-if arg not in VALID_ARGUMENTS:
-  raise ValueError(f"invalid argument: {arg}")
 
 match arg:
-  case "run_once":
+  case VALID_ARGUMENTS.RUN_ONCE:
     run_azb()
-  case "run_daily":
+  case VALID_ARGUMENTS.RUN_DAILY:
     pass
-  case "run_when_dirs_available":
+  case VALID_ARGUMENTS.RUN_WHEN_DIRS_AVAILABLE:
     pass
+  case VALID_ARGUMENTS.CLI:
+    pass
+  case VALID_ARGUMENTS.GUI:
+    pass
+  case _:
+    raise ValueError(f"invalid argument: {arg}")
 
 print("AZB End.")
 input()
